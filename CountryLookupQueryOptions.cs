a465e0205ccb35ea071062b8528baba2e1b792f2
APIVerve Team <hello@apiverve.com>
2024-08-20 10:47:48 -0500

    Bug Fixes

---
+++ CountryLookupQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class CountryLookupQueryOptions {
+public string country { get; set; }
+}
+
+}
